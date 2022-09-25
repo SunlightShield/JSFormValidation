c9acfcd61be59d73600b78d3721a3d640214547e
sebastian <sespinozahenriquez@gmail.com>
2022-09-25 14:07:37 -0300

    añadido un css basico

---
+++ index.js
@@ -0,0 +1,6 @@
+//primero, llamamos cada elemento por su ID
+const form = document.getElementById('form');
+const usuario = document.getElementById('usuario');
+const email = document.getElementById('email');
+const contrasena = document.getElementById('contrasena');
+const contrasena2 = document.getElementById('contrasena2');
